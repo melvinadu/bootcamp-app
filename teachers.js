a8d14372f6b92c81e2073b2e531a87f22d952e9a
Melvin Adu <contactmelvinadu@gmail.com>
2022-01-06 18:39:23 +0000

    add parameterized queries to queries

--- teachers.js
+++ teachers.js
@@ -7,17 +7,24 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON students.id = student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${process.argv[2]}'
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`);
-  })
-});
+`;
+
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array.
+const values = [`${cohortName}`];
+
+pool.
+  query(queryString, values)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.cohort}: ${user.teacher}`);
+    })
+  });
